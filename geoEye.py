74a342c995002a31d8deccb8e710f95d4fc3a78a
Ori <45724082+therealOri@users.noreply.github.com>
2021-08-23 17:23:14 -0600

    Update geoEye.py

--- geoEye.py
+++ geoEye.py
@@ -8,6 +8,6 @@ response = re.get(url)
 
 # This isn't very helpful as the data gathered can be longer or shorter which results in valuable data being cut off at idk where. Anyone is more than welcome to push a request and change this code to hopefully make it better and more useful.
 # I need a better way to either filter out everything I don't need, or to find a way to specifically grab what I need from the url/response then print it.
-# As of rn, the values below should be enough to give you a lattitude, a longitude, country name/location and maybe a roadname and or a road number. You can change this if you need.
+# As of rn, the values below should be enough to give you a lattitude, a longitude, town name/location and maybe a road name and or a road number. You can change this if you need.
 res = response.text[200:420]
 print(res.replace("null", ""))
