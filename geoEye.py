d0bd9ba1debcc5b1aa3236502b2c26c791cc09e6
Ori <45724082+therealOri@users.noreply.github.com>
2023-11-22 09:31:45 -0700

    Update geoEye.py
    Added a better way to asking for input and also improved the option to open the cords up in a browser.

--- geoEye.py
+++ geoEye.py
@@ -1,15 +1,50 @@
 import os
 import requests
 import re
+import beaupy
+from beaupy.spinners import *
+import shutil
 
 
-url = input('URL Here: ')
-response = requests.get(url)
+def clear():
+	os.system("clear||cls")
 
-res = response.text[200:420].replace("null","")
 
-pattern = r"-*\d+\.\d+,-*\d+\.\d+"
-matches = re.findall(pattern,res)[0].split(',')
+def main():
+	url = beaupy.prompt("Url: ")
+	clear()
+	spinner = Spinner(DOTS, "Searching for latitude & longitude...")
+	spinner.start()
 
-print(matches)
-# os.system(f"firefox 'https://www.google.com/maps/place/{matches[0],matches[1]}'")
+	response = requests.get(url)
+	res = response.text[200:420].replace("null","")
+	pattern = r"-*\d+\.\d+,-*\d+\.\d+"
+	matches = re.findall(pattern,res)[0].split(',')
+	spinner.stop()
+
+	return matches
+
+
+
+
+if __name__ == '__main__':
+	clear()
+	cords = main()
+	choice = beaupy.confirm("Do you want to open google maps with the cords?")
+
+	if not choice:
+		clear()
+		print(cords)
+	else:
+		clear()
+		print(cords)
+		browsers = ["firefox", "chrome", "brave", "safari", "msedge"]
+		default_browser = "xdg-open"
+		for browser in browsers:
+			if shutil.which(browser):
+				os.system(f"{browser} 'https://www.google.com/maps/place/{cords[0], cords[1]}'")
+				break
+		else:
+			os.system(f"{default_browser} 'https://www.google.com/maps/place/{cords[0], cords[1]}'")
+
+
